3dd1fe7489b064528bafdcf31d43b201eeb73721
hermanbayron <foforito2001gg@gmail.com>
2022-10-04 14:53:55 +0000

    Ejercicio React con JS+HTML

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+// create your first component
+const Footer = () => {
+	return (
+		<div>
+            <nav className="navbar navbar-dark bg-dark d-flex justify-content-center">
+                <h5 className="text-white bg-dark">Copyright @ Your website 2013</h5>
+            </nav>
+        </div>
+	);
+};
+
+export default Footer;
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+//create your first component
+const Jumbotron = () => {
+	return (
+<div className="d-flex justify-content-center">
+    <div className="jumbotron d-block">
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4"/>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+</div>
+  );
+};
+
+export default Jumbotron;
